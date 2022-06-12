2d7c31df938e1324ffa0a4ecea4424c1e5786d88
GokeAdewuyi <adewuyiyusuf@yahoo.com>
2022-06-12 17:44:17 +0100

    fix bug in helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -13,13 +13,13 @@ if (!function_exists("get_current_semester")) {
 if (!function_exists("get_current_semester_status")) {
     function get_current_semester_status() {
         if (session('current_semester'))
-            return Semester::find(session('current_semester'))->status;
-        return Semester::where('status', 'current')->first()->status;
+            return Semester::find(session('current_semester'))->status ?? null;
+        return Semester::where('status', 'current')->first()->status ?? null;
     }
 }
 
 if (!function_exists("get_current_semester_id")) {
     function get_current_semester_id() {
-        return session('current_semester') ?? Semester::where('status', 'current')->first()->id;
+        return session('current_semester') ?? Semester::where('status', 'current')->first()->id ?? null;
     }
 }
