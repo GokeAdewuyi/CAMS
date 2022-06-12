3d5855122d64b2a15388ca1bd744995d65f9604a
GokeAdewuyi <adewuyiyusuf@yahoo.com>
2022-06-12 18:31:33 +0100

    fix issues in admin course table

--- resources/views/admin/courses/table-actions.blade.php
+++ resources/views/admin/courses/table-actions.blade.php
@@ -2,52 +2,6 @@
     $course = App\Models\Course::where('id', $id)->first();
 @endphp
 <div class="flex space-x-1 justify-around">
-{{--    <x-modal :value="$id">--}}
-{{--        <x-slot name="trigger">--}}
-{{--            <button class="p-1 text-blue-600 hover:bg-blue-600 hover:text-white rounded">--}}
-{{--                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path><path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd"></path></svg>--}}
-{{--            </button>--}}
-{{--        </x-slot>--}}
-{{--        <x-slot name="header">--}}
-{{--            {{ __('Course Details') }}--}}
-{{--        </x-slot>--}}
-{{--        <x-slot name="content">--}}
-{{--            <div class="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">--}}
-{{--                <table class="w-full">--}}
-{{--                    <tr>--}}
-{{--                        <td class="w-1/3"><b>Code:</b></td>--}}
-{{--                        <td class="w-2/3 left">{{ $course['code'] }}</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td class="w-1/3"><b>Title:</b></td>--}}
-{{--                        <td class="w-2/3">{{ $course['title'] }}</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td class="w-1/3"><b>Unit:</b></td>--}}
-{{--                        <td class="w-2/3">{{ $course['unit'] }}</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td class="w-1/3"><b>Curriculum:</b></td>--}}
-{{--                        <td class="w-2/3">{{ $course->curriculum->name }}</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td class="w-1/3"><b>Date Added:</b></td>--}}
-{{--                        <td class="w-2/3">{{ date('Y-m-d', strtotime($course['created_at'])) }}</td>--}}
-{{--                    </tr>--}}
-{{--                    <tr>--}}
-{{--                        <td class="w-1/3"><b>Total Lecturers:</b></td>--}}
-{{--                        <td class="w-2/3">--}}
-{{--                            @if(count($course->lecturers) > 0)--}}
-{{--                                {{ $course->lecturers()->count() }}--}}
-{{--                            @else--}}
-{{--                                <i class="text-blue-400">Course not allocated yet</i>--}}
-{{--                            @endif--}}
-{{--                        </td>--}}
-{{--                    </tr>--}}
-{{--                </table>--}}
-{{--            </div>--}}
-{{--        </x-slot>--}}
-{{--    </x-modal>--}}
     <x-modal :value="$id" :open="old('course') == $id">
         <x-slot name="trigger">
             <button class="p-1 text-blue-600 hover:bg-blue-600 hover:text-white rounded" title="Allocate Course">
@@ -79,7 +33,7 @@
                         <x-jet-label for="" value="{{ __('Lecturers*') }}" />
                         <x-jet-input-error for="lecturers" class="" />
                         <div class="grid grid-cols-2 gap-2" id="lecturers-list-{{ $course['id'] }}">
-                            @foreach(\App\Models\User::where('id', '!=', 1)->orderBy('name')->get() as $key => $lecturer)
+                            @foreach(\App\Models\User::where('is_admin', false)->orderBy('name')->get() as $key => $lecturer)
                                 <div>
                                     <x-jet-label for="lecturer-{{ $course['id'] }}-{{ $key }}">
                                         <div class="flex items-center">
--- resources/views/livewire/courses/table-actions.blade.php
+++ resources/views/livewire/courses/table-actions.blade.php
@@ -61,19 +61,3 @@
         </a>
     @endif
 </div>
-
-{{--@section('styles')--}}
-{{--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">--}}
-{{--    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">--}}
-{{--@endsection--}}
-{{--@section('scripts')--}}
-{{--    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>--}}
-{{--    <script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"></script>--}}
-{{--    <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>--}}
-{{--    <script>--}}
-{{--        $('.datatable').DataTable({--}}
-{{--            "lengthMenu": [ [10, 20, 50, 100], [10, 20, 50, 100] ],--}}
-{{--            "pageLength": 10--}}
-{{--        })--}}
-{{--    </script>--}}
-{{--@endsection--}}
