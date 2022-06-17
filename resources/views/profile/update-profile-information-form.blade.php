b1eb092b0f5dc0a5085cfbda34f71038e7f23f21
GokeAdewuyi <adewuyiyusuf@yahoo.com>
2022-06-17 08:11:31 +0100

    update

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -85,7 +85,7 @@
         <!-- PFN -->
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="pfn" value="{{ __('PFN') }}" />
-            <x-jet-input id="pfn" type="text" class="mt-1 block w-full" wire:model.defer="state.pfn" readonly="{{ !auth()->user()['is_admin'] ? 'readonly' : false }}" />
+            <x-jet-input id="pfn" type="text" class="mt-1 block w-full" wire:model.defer="state.pfn" {{ !auth()->user()['is_admin'] ? 'readonly="readonly"' : '' }} />
             <x-jet-input-error for="pfn" class="mt-2" />
         </div>
     </x-slot>
