c284b985a4c03791286bf8243416351e68806094
GokeAdewuyi <adewuyiyusuf@yahoo.com>
2022-06-17 08:50:14 +0100

    update

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -37,6 +37,7 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'pfn' => $input['pfn'] ?? $user['pfn']
             ])->save();
         }
     }
